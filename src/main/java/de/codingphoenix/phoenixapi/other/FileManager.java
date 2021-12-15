1bcbd36421b3c334499bddfdac828e705b7f3b61
Jan <jan.f.meinl@web.de>
2021-12-15 10:59:36 +0100

    Added FileManager.java

---
+++ src/main/java/de/codingphoenix/phoenixapi/other/FileManager.java
@@ -0,0 +1,24 @@
+package de.codingphoenix.phoenixapi.other;
+
+import java.io.File;
+import java.net.URLDecoder;
+import java.security.CodeSource;
+
+public class FileManager {
+    public static String getJarContainingFolder(Class aclass) throws Exception {
+        CodeSource codeSource = aclass.getProtectionDomain().getCodeSource();
+
+        File jarFile;
+
+        if (codeSource.getLocation() != null) {
+            jarFile = new File(codeSource.getLocation().toURI());
+        }
+        else {
+            String path = aclass.getResource(aclass.getSimpleName() + ".class").getPath();
+            String jarFilePath = path.substring(path.indexOf(":") + 1, path.indexOf("!"));
+            jarFilePath = URLDecoder.decode(jarFilePath, "UTF-8");
+            jarFile = new File(jarFilePath);
+        }
+        return jarFile.getParentFile().getAbsolutePath();
+    }
+}
